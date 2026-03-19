76b8dd672b7c2754eab701c5b3a15da6309ca528
Lorenzo <lorenzo.baravelli0603@gmail.com>
2026-03-19 15:44:13 +0100

    Fine programma, aggiunta della txtBox per inserire il numero di lettere possibili, aggiunta listBox, risultato desiderato raggiunto

--- estrazioneLettera/EstrazioneLettera/MainWindow.xaml.cs
+++ estrazioneLettera/EstrazioneLettera/MainWindow.xaml.cs
@@ -22,7 +22,6 @@ namespace estrazioneLettere_ProgrammazioneAsincrona
             InitializeComponent();
             LetteraEstratta();
             RotazioneLettera();
-
         }
 
         private string[] elencoLettere = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
@@ -30,6 +29,8 @@ namespace estrazioneLettere_ProgrammazioneAsincrona
         Random rnd = new Random();
 
         string letteraEstratta;
+        string parolaEstratta;
+        int numeroLettereEstratte;
 
         private async void LetteraEstratta()
         {
@@ -41,37 +42,83 @@ namespace estrazioneLettere_ProgrammazioneAsincrona
             }
         }
 
+
+
         private async void RotazioneLettera()
         {
-            
-                this.Dispatcher.BeginInvoke(new Action(async () =>
+
+            this.Dispatcher.BeginInvoke(new Action(async () =>
+            {
+                while (true)
                 {
-                    while (true)
-                    {
-                        btnEstrazione.Content = letteraEstratta;
-                        await Task.Delay(100);
-                    }
-                }));
-            
+                    btnEstrazione.Content = letteraEstratta;
+                    await Task.Delay(100);
+                }
+            }));
+
         }
 
-        private void btnEstrazione_Click(object sender, RoutedEventArgs e)
+        // funzione che assegna al numero delle lettere estratte il valore che vuole l'utente
+
+        // mettiamo un range perché se troppo alto il programma potrebbe bloccarsi
+        private void NumeroLettere_Click(object sender, RoutedEventArgs e)
         {
 
+            string testo = txtBoxNumeriEstratti.Text;
+
+            // per fare dei controlli che il testo sia un numero intero
+            // out mi permette di mettere in uscita anche un numero intero
+
+            if (int.TryParse(testo, out int numero))
+            {
+                if (numero >= 1 && numero <= 20)
+                {
+                    numeroLettereEstratte = numero;
+                }
+                else
+                {
+                    MessageBox.Show("Il numero intero non rientra nel range di valori da 1 a 20");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Non è un numero intero valido");
+            }
+        }
+
+
+        private async void btnEstrazione_Click(object sender, RoutedEventArgs e)
+        {
 
             Task.Run(() => {
-                this.Dispatcher.BeginInvoke(new Action(() =>
+                this.Dispatcher.BeginInvoke(new Action(async () =>
                 {
+                    if (numeroLettereEstratte != 0)
+                    {
+                        for (int i = 0; i < numeroLettereEstratte; i++)
+                        {
+                            parolaEstratta = parolaEstratta + letteraEstratta;
+
+                            await Task.Delay(100);
+
+                        }
+
+                        lbxLettereEstratte.Items.Add(parolaEstratta);
+
+                        parolaEstratta = "";
+                    } else
+                    {
+                        MessageBox.Show("Non è stato inserito alcun tipo di numero valido");
+                    }
                     
-                        
-                    textBoxEstratti.Text = textBoxEstratti.Text + letteraEstratta;
-                    // &#10; per andare a capo nella textBox
 
                 }));
             });
 
         }
 
-        
+
+
+
     }
 }
